3af896f92d12c9d8ef425abc42ed212eb082416c
BradleyParkerDev <bradleyscottparker@outlook.com>
2024-04-02 07:49:51 -0500

    refresh token is rotated in cookies w/ access token

--- news-app-client/src/lib/auth/fetchAccessToken.js
+++ news-app-client/src/lib/auth/fetchAccessToken.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import setHeaderToken from "./setHeaderToken";
 import Cookies from "universal-cookie";
+import logoutUser from "./logoutUser";
 import { jwtDecode } from "jwt-decode";
 
 const urlEndPoint = process.env.REACT_APP_BASE_URL;
@@ -13,6 +14,7 @@ const fetchAccessToken = async () => {
 
         if (!accessToken && !refreshToken) {
             console.log('Neither access token nor refresh token found in cookies.');
+            localStorage.removeItem('user')
             return;
         }
 
@@ -20,26 +22,22 @@ const fetchAccessToken = async () => {
             console.log('Access token not found in cookies. Refreshing...');
             const response = await axios.post(`${urlEndPoint}/users/refresh-access-token`, { refreshToken });
             accessToken = response.data.accessToken;
+            refreshToken = response.data.refreshToken;
 
-            if (!accessToken) {
-                console.error('Error refreshing access token: Access token is null or undefined.');
-                return;
-            }
-
-            const decodedToken = jwtDecode(accessToken);
+            const decodedAccessToken = jwtDecode(accessToken);
+            const decodedRefreshToken = jwtDecode(refreshToken);
 
             // Calculate the expiration time in milliseconds
-            const expirationTime = (decodedToken.exp - decodedToken.iat) * 1000;
-            console.log(expirationTime)
+            const accessTokenExp = (decodedAccessToken.exp - decodedAccessToken.iat) * 1000;
             // Set the access token cookie with expiration time
-            cookies.set('accessToken', accessToken, { expires: new Date(Date.now() + expirationTime) });
+            cookies.set('accessToken', accessToken, { expires: new Date(Date.now() + accessTokenExp) });
+
+            // Calculate the expiration time in milliseconds
+            const refreshTokenExp = (decodedRefreshToken.exp - decodedRefreshToken.iat) * 1000;
+            // Set the refresh token cookie with expiration time
+            cookies.set('refreshToken', refreshToken, { expires: new Date(Date.now() + refreshTokenExp) });
         } else {
             console.log('Using access token from cookies.');
-        }
-
-        // Set the refresh token cookie if it exists
-        if (refreshToken) {
-            cookies.set('refreshToken', refreshToken);
         }
 
         setHeaderToken(accessToken);
@@ -47,6 +45,10 @@ const fetchAccessToken = async () => {
         return accessToken;
     } catch (error) {
         console.error('Error fetching/accessing access token:', error);
+        if (error.response && error.response.status === 403) {
+            console.log('Invalid refresh token. Logging out...');
+            localStorage.removeItem('user')
+        }
         throw error; // Re-throw the error to handle it in the calling function
     }
 };
--- news-app-client/src/lib/auth/setHeaderToken.js
+++ news-app-client/src/lib/auth/setHeaderToken.js
@@ -5,7 +5,6 @@ const setHeaderToken = (token) => {
     if(token){
         axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
         console.log('Authorization headers set!')
-        console.log(token)
     }else{
         delete axios.defaults.headers.common['Authorization'];
     }
