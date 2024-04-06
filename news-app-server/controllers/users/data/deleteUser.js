c9248934e36d8a1237074dbfa57ab9707ba3310b
BradleyParkerDev <bradleyscottparker@outlook.com>
2024-04-06 00:37:47 -0500

    updateUser and deletUser functions work

--- news-app-server/controllers/users/data/deleteUser.js
+++ news-app-server/controllers/users/data/deleteUser.js
@@ -1,7 +1,25 @@
+const User = require('../../../models/Users')
+
 //Delete
 const deleteUser = async (req,res) => {
 
+    try {
+        const id = req.decoded.userData.userId
 
+        console.log(id)
+        const response = await User.deleteOne({id:id})   
+
+        // Check if the response indicates that a document was deleted
+        if (response.deletedCount === 0) {
+            return res.status(404).json({ success: false, message: 'User not found' });
+        }
+        
+        return res.status(200).json({success:true, message: 'User successfully deleted.', response: response})
+
+    } catch (error) {
+        console.log('Error deleting user', error);
+        return res.status(500).json({ success: false, message: 'Error deleting user on the server.' });        
+    }
 
 }
 
