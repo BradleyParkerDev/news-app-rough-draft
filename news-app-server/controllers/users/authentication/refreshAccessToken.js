c373e8032322688b97e1f903b133648040cfaf1b
BradleyParkerDev <bradleyscottparker@outlook.com>
2024-04-11 03:49:38 -0500

    refactoring refreshAccessToken, miticated some of the errors by checking if the oldRefreshToken was undefined before going through the function

--- news-app-server/controllers/users/authentication/refreshAccessToken.js
+++ news-app-server/controllers/users/authentication/refreshAccessToken.js
@@ -1,34 +1,38 @@
-const User = require('../../../models/Users');
-const {checkUserRefreshTokens} = require('../../../auth')
+const { checkUserRefreshTokens } = require('../../../auth');
 
 const refreshAccessToken = async (req, res) => {
     const oldRefreshToken = req.body.refreshToken;
-    try {
 
-        console.log(`Old refresh token ${oldRefreshToken}`)
-        const{
-            newRefreshToken,
-            refreshTokenExpiration,
-            accessToken,
-            accessTokenExpiration
-        } = await checkUserRefreshTokens(oldRefreshToken,res)
+    if (oldRefreshToken) {
+        try {
+            const {
+                newRefreshToken,
+                refreshTokenExpiration,
+                accessToken,
+                accessTokenExpiration
+            } = await checkUserRefreshTokens(oldRefreshToken, res);
 
-        console.log(`New refresh token ${newRefreshToken}`)
-
-        
-        res.json({ 
-            success: true, 
-            message: 'New refresh and access tokens sent in cookies.', 
-            newRefreshToken,
-            refreshTokenExpiration,
-            accessToken,
-            accessTokenExpiration
+            res.json({ 
+                success: true, 
+                message: 'New refresh and access tokens sent in cookies.', 
+                newRefreshToken,
+                refreshTokenExpiration,
+                accessToken,
+                accessTokenExpiration
+            });
+        } catch (error) {
+            console.error('Error refreshing access token:', error);
+            res.status(500).json({ 
+                success: false, 
+                message: 'Internal server error', 
+                error: error.message // Include error message in response
+            });
+        }
+    } else {
+        res.status(400).json({
+            success: false,
+            message: 'Old refresh token is missing in the request body.'
         });
-
-
-    } catch (error) {
-        console.error('Error refreshing access token:', error);
-        res.status(500).json({ success: false, message: 'Internal server error' });
     }
 };
 
